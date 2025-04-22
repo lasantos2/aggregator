1361d32f4169b4a0e2d3095f3016fa1b26e72091
AppleTruffle <appletrufflemuffinkinz@gmail.com>
2025-04-22 00:11:52 -0600

    middleware for login commands

--- internal/database/users.sql.go
+++ internal/database/users.sql.go
@@ -165,7 +165,7 @@ SELECT feed_follows.id, feed_follows.created_at, feed_follows.updated_at, feed_i
 FROM feed_follows
 INNER JOIN users ON feed_follows.user_id = users.id
 INNER JOIN feeds ON feed_follows.feed_id = feeds.id
-WHERE users.name = $1
+WHERE users.id = $1
 `
 
 type GetFeedFollowsForUserRow struct {
@@ -188,8 +188,8 @@ type GetFeedFollowsForUserRow struct {
 	UserName    string
 }
 
-func (q *Queries) GetFeedFollowsForUser(ctx context.Context, name string) ([]GetFeedFollowsForUserRow, error) {
-	rows, err := q.db.QueryContext(ctx, getFeedFollowsForUser, name)
+func (q *Queries) GetFeedFollowsForUser(ctx context.Context, id uuid.UUID) ([]GetFeedFollowsForUserRow, error) {
+	rows, err := q.db.QueryContext(ctx, getFeedFollowsForUser, id)
 	if err != nil {
 		return nil, err
 	}
--- sql/queries/users.sql
+++ sql/queries/users.sql
@@ -57,4 +57,4 @@ SELECT *, feeds.name AS feed_name, users.name AS user_name
 FROM feed_follows
 INNER JOIN users ON feed_follows.user_id = users.id
 INNER JOIN feeds ON feed_follows.feed_id = feeds.id
-WHERE users.name = $1;
+WHERE users.id = $1;
