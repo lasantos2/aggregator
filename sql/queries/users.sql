f5c35e9af252d87d2743a243c5ccba58b9d3f1bb
AppleTruffle <appletrufflemuffinkinz@gmail.com>
2025-04-21 23:37:43 -0600

    Finished the featured of following

--- sql/queries/users.sql
+++ sql/queries/users.sql
@@ -14,6 +14,8 @@ WHERE name = $1;
 
 -- name: Reset :exec
 DELETE FROM users;
+DELETE FROM feeds;
+DELETE FROM feed_follows;
 
 -- name: GetUsers :many
 SELECT * FROM users;
@@ -37,4 +39,22 @@ SELECT * FROM feeds;
 SELECT users.name FROM users JOIN feeds ON users.id = $1;
 
 -- name: CreateFeedFollow :many
-INSERT INTO feed_follows ();
+WITH inserted_feed_follow AS (
+INSERT INTO feed_follows (id, created_at, updated_at, feed_id, user_id)
+VALUES ( $1,$2,$3,$4,$5)
+RETURNING *
+)
+SELECT 
+inserted_feed_follow.*,
+feeds.name AS feed_name,
+users.name AS user_name
+FROM inserted_feed_follow 
+INNER JOIN users ON inserted_feed_follow.user_id = users.id
+INNER JOIN feeds ON inserted_feed_follow.feed_id = feeds.id;
+
+-- name: GetFeedFollowsForUser :many
+SELECT *, feeds.name AS feed_name, users.name AS user_name 
+FROM feed_follows
+INNER JOIN users ON feed_follows.user_id = users.id
+INNER JOIN feeds ON feed_follows.feed_id = feeds.id
+WHERE users.name = $1;
